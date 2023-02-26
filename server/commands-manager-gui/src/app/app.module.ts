78b2249af663088a1d59b222fa8606f267d298d7
szarkii <szarkii@protonmail.com>
2023-02-26 17:55:55 +0100

    Add grid

--- server/commands-manager-gui/src/app/app.module.ts
+++ server/commands-manager-gui/src/app/app.module.ts
@@ -2,22 +2,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PanelModule } from 'primeng/panel';
 import { TreeModule } from 'primeng/tree';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroupsListComponent } from './groups/groups-list/groups-list.component';
+import { CommandDetailsComponent } from './command/command-details/command-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroupsListComponent
+    GroupsListComponent,
+    CommandDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    TreeModule
+    TreeModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ server/commands-manager-gui/src/app/command/command-details/command-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-command-details',
+  templateUrl: './command-details.component.html',
+  styleUrls: ['./command-details.component.css']
+})
+export class CommandDetailsComponent {
+
+}
